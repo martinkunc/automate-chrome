ba121b3aa5287c5cffdb7ee94bcc10ab2567e746
Martin Kunc <martin.kunc@windowslive.com>
2013-09-12 08:18:17 +0200

    Initial commit

---
+++ AutomateChrome/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChromeAutomation;
+
+namespace AutomateChrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chrome = new Chrome("http://localhost:9222");
+            
+            var sessions = chrome.GetAvailableSessions();
+            
+            Console.WriteLine("Available debugging sessions");
+            foreach (var s in sessions)
+                Console.WriteLine(s.url);
+
+            if (sessions.Count == 0)
+                throw new Exception("All debugging sessions are taken.");
+            
+            // Will drive first tab session
+            var sessionWSEndpoint = 
+                sessions[0].webSocketDebuggerUrl;
+
+            chrome.SetActiveSession(sessionWSEndpoint);
+
+            chrome.NavigateTo("http://www.google.com");
+
+            var result = chrome.Eval("document.getElementById('lst-ib').value='Hello World'");
+
+            result = chrome.Eval("document.forms[0].submit()");
+
+            Console.ReadLine();
+        }
+    }
+}
